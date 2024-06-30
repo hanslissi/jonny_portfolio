a4ed69e25d7da6fced80cf4d4f2cee5e282beec0
hanslissi <riedmueller.jo@gmail.com>
2024-06-30 22:53:17 +0200

    add playground to nav

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -20,30 +20,36 @@ const Nav = ({ backbuttonInfo }: NavProps) => {
             {backbuttonInfo.title}
           </Link>
         )}
-        <ul className="absolute top-0 left-1/2 -translate-x-1/2 bg-brightSecondaryBg backdrop-blur-md border-[0.5px] rounded-full flex flex-row justify-center items-center px-1 py-1 gap-2">
+        <ul className="absolute top-0 left-1/2 -translate-x-1/2 bg-brightSecondaryBg backdrop-blur-md border-[0.5px] rounded-full flex flex-row justify-center items-center p-1 gap-2">
           <Link
             to="/#home"
-            className="px-4 py-1 rounded-full text-nowrap hover:bg-whiteHighlight"
+            className="px-4 py-2 rounded-full text-nowrap hover:bg-whiteHighlight"
           >
             Home
           </Link>
           <Link
             to="/#about-me"
-            className="px-4 py-1 rounded-full text-nowrap hover:bg-whiteHighlight"
+            className="px-4 py-2 rounded-full text-nowrap hover:bg-whiteHighlight"
           >
             About Me
           </Link>
           <Link
             to="/#projects"
-            className="px-4 py-1 rounded-full text-nowrap hover:bg-whiteHighlight"
+            className="px-4 py-2 rounded-full text-nowrap hover:bg-whiteHighlight"
           >
             Projects
           </Link>
           <Link
             to="/#contact"
-            className="px-4 py-1 rounded-full text-nowrap hover:bg-whiteHighlight"
+            className="px-4 py-2 rounded-full text-nowrap hover:bg-whiteHighlight"
           >
             Contact
+          </Link>
+          <Link
+            to="/playground"
+            className="px-4 py-2 text-white rounded-full text-nowrap bg-primary hover:bg-secondary"
+          >
+            Playground
           </Link>
         </ul>
       </nav>
